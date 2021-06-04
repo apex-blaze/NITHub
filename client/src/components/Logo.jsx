8ca245b03231126334d741d306d4c48b03d25055
Sumit047 <sumitsk020@gmail.com>
2021-06-04 17:02:28 +0530

    Navbar Created

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Navbar.css"
+import logo from "../images/logo.png";
+function Logo() {
+    return (
+        
+       <img src={logo} alt="logo" className="navlogo"/>
+     
+    );
+}
+
+export default Logo;
---
+++ client/src/components/Navlist.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Navbar.css"
+function Navlist()
+{
+    return(
+        <div className="navlist">
+            <a href="/" >Home</a>
+            <a href="/" >Support</a>
+            <a href="/" >Contact</a>
+            <a href="/" >About Us</a>
+        </div>
+    );
+}
+export default Navlist;
