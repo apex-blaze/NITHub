e9d7de317c8908cc3f3df57d7a28270c1701cce1
apex-blaze <ritish619@gmail.com>
2021-06-09 15:08:15 +0530

    linked dashboard to register

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import register from "./components/Register"
+import Register from "./components/Register";
 // import Footer from "./components/Footer";
-import Dashboard from "./components/Dashboard"
+import Dashboard from "./components/Dashboard";
 const App1 = () => (
   <BrowserRouter>
-    <Switch>      
+    <Switch>
       <Route path="/" exact component={App} />
-      <Route exact path="/register" component={register}/>
+      <Route exact path="/register" component={Register} />
       <Route exact path="/dashboard" component={Dashboard} />
     </Switch>
   </BrowserRouter>
