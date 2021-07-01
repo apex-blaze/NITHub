2448e2cfb71b86de8bad4d3aa307ba5c532eb08c
apex-blaze <ritish619@gmail.com>
2021-07-01 17:34:12 +0530

    finalised build

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use("/", express.static("public"));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://nithub.netlify.app/",
     credentials: true,
   })
 );
